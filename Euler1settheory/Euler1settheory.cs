61242d1a36e6a071dfbb66835a0292decb3c0eef
ccnelson <ccnelson@mail.com>
2019-05-07 20:19:20 +0100

    second

--- Euler1settheory/Euler1settheory.cs
+++ Euler1settheory/Euler1settheory.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+This version uses set theory to reduce calculations, but due
+to c#'s short circuit 'OR' (||) this is mostly slower */
+
+using System;
 using System.Diagnostics;
 
 namespace Euler1settheoryname
