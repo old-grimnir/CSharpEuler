8a064ee0e8ebaaf2b2532f3bb25ff3f65c4cd0e9
ccnelson <ccnelson@mail.com>
2019-05-07 21:38:13 +0100

    formatting

---
+++ Euler4/Euler4.cs
@@ -0,0 +1,27 @@
+﻿/*A palindromic number reads the same both ways. 
+The largest palindrome made from the product of 
+two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product 
+of two 3-digit numbers. */
+
+using System;
+
+namespace Euler4name
+{
+    class Euler4
+    {
+        static int revnum(int target) // math method to reverse a number (beware it ditches leading zeroes)
+        {
+            int sum = 0;
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
