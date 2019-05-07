7af4657e672a95c081755b9a8f779e585d526218
ccnelson <ccnelson@mail.com>
2019-05-07 18:22:58 +0100

    first

---
+++ Euler1/Euler1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Euler1name
+{
+    class Euler1
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            int total = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))  // short circuited 'OR' outperforms set theory
+                {
+                    total += i;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", total);
+            Console.WriteLine("Took: {0}", watch.ElapsedTicks);
+        }
+    }
+}
---
+++ Euler1settheory/Euler1settheory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Euler1settheoryname
+{
+    class Euler1settheory
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            double sumofthrees = 0;
+            double sumoffives = 0;
+            double sumoffifteens = 0;
+            double finalanswer = 0;
+            watch.Start();
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0) { sumofthrees += i; }
+                if (i % 5 == 0) { sumoffives += i; }
+                if (i % 15 == 0) { sumoffifteens += i; }
+                finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+            }
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", finalanswer);
+            Console.WriteLine("Took: {0}", watch.ElapsedTicks);
+        }
+    }
+}
