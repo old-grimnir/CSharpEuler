563b9d1918b8d874459103b07f7b80cf2e49e14b
bob <bob@DESKTOP-93R9I4T>
2019-05-10 02:46:19 +0100

    pf works

--- Scratch/Scratch.cs
+++ Scratch/Scratch.cs
@@ -5,38 +5,44 @@ namespace Scratchname
 {
     class Scratch
     {
-        static Boolean primefunc(int target) // methods before main
+        static Boolean primefunc(int inputNo) // methods before main
         {
-            if (target <= 1) return false; // 1 isnt prime
-            if (target == 2) return true; // 2 is prime
-            if (target % 2 == 0) return false;
-            var boundary = (int)Math.Floor(Math.Sqrt(target));
+            if (inputNo <= 1) return false; // 1 isnt prime
+            if (inputNo == 2) return true; // 2 is prime
+            if (inputNo % 2 == 0) return false;
+            var boundary = (int)Math.Floor(Math.Sqrt(inputNo));
             for (int i = 3; i <= boundary; i += 2) // increment thru odds
             {
-                if (target % i == 0) return false;
+                if (inputNo % i == 0) return false;
             }
             return true;
         }
 
         static List<long> primefactors(List<long> bob, int target)
         {
-            for (int i = 1; i < target; i++)
+            for (int i = 2; i < target; i++)
             {
                 if (target % i == 0 & primefunc(i) == true) // check if prime factor
                 {
                     bob.Add(i); // add to list
+                    target = target / i;
+                    i = 1; // reset as we just divided
+                    if (primefunc(target) == true) // check if target yet prime
+                    {
+                        bob.Add(target); // add to list
+                        break; // reached end
+                    }
                 }
             }
-            return bob;
+            return bob; // return list
         }
 
 
         static void Main(string[] args)
         {
-            List<long> pflist20 = new List<long>(); // make a list of prime factors
-            pflist20 = (primefactors(pflist20, 12));
-            foreach (var m in pflist20) { Console.WriteLine(m); }
-
+            List<long> pflist1 = new List<long>(); // make a list of prime factors
+            pflist1 = (primefactors(pflist1, 48));
+            foreach (var m in pflist1) { Console.Write("{0}, ", m); }
         }
     }
 }
