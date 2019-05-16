4da84e6bc22618ef67d11f7cf6a994e1b4abcbea
ccnelson <ccnelson@mail.com>
2019-05-16 20:27:09 +0100

    added new prob 10

---
+++ Euler10/Euler10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Euler10name
+{
+    class Euler10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
