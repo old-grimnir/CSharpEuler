dd19a36fc08f37ec0a46cb24855c66d38419b652
ccnelson <ccnelson@mail.com>
2019-05-16 18:28:45 +0100

    new

--- Scratch/Scratch.cs
+++ Scratch/Scratch.cs
@@ -6,11 +6,28 @@ namespace Scratchname
 {
     class Scratch
     {
+        private static ulong GCD(ulong a, ulong b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a == 0 ? b : a;
+        }
+
+
+
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("hello jupiter!");
+
+            //Console.WriteLine(GCD(270, 192));
             // try prob 5 using Euclid's algorithm
+
+            Console.WriteLine(GCD(270, 192));
         }
     }
 }
