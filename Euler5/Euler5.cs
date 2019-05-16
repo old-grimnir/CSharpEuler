edad60d72ad74436bfd2256441f9b795f47822d7
ccnelson <ccnelson@mail.com>
2019-05-16 15:15:07 +0100

    prob five works (scratch2)

--- Euler5/Euler5.cs
+++ Euler5/Euler5.cs
@@ -3,39 +3,9 @@ each of the numbers from 1 to 10 without any remainder.
 what is the smallest positive number that is evenly 
 divisible by all of the numbers from 1 to 20?
 (Answer: 232792560) */
-
-// this is bad (2760227 ticks) 
-// a better approach would be LCM of a set
-// find prime factors  of 1-20
-// ???
-// profit
-// http://www.math.com/school/subject1/lessons/S1U3L3DP.html
-//
-// go through list of numbers
-// create list of prime factors & list of greatest occurences
-// multiply each number according to its greatest number of occurences
-// i.e 
-// 30 = 2 * 3 * 5
-// 45 = 3 * 3 * 5
-// LCM = 2 * 3 * 3 * 5 = 90
-// do magic
-// get answer
-
-// create prime checker
-// look for prime factors
-// use a list
-// index of list is number being checked
-// content at index is a list of prime factors
-// ( 0 would be empty)
-// make another list of occurences
-// might need a dict for this
-// prime fact : occurences
-// find most occurences
-// multiply em all
-// find answer
-
-
-
+// this is not ideal ( 210107 ticks) 
+// option A: prime factors to find LCM
+// option B: find GCD to find LCM
 
 using System;
 using System.Diagnostics;
@@ -47,12 +17,12 @@ namespace Euler5name
         static void Main(string[] args)
         {
             var watch = new System.Diagnostics.Stopwatch();
-            int i = 1;
+            int i = 20;
             watch.Start();
-            while (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 || i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 || i % 10 != 0 || i % 11 != 0 ||
+            while (i % 3 != 0 || i % 4 != 0 || i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 || i % 11 != 0 ||
             i % 12 != 0 || i % 13 != 0 || i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 || i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
             {
-                i++;
+                i += 20; // increments of 20, highest number we need divisable. removed 2, 5, and 10. 
             }
             watch.Stop();
             Console.WriteLine("Answer: {0}", i);
--- Scratch2/Scratch2.cs
+++ Scratch2/Scratch2.cs
@@ -29,14 +29,13 @@ namespace Scratch2name
             public Coords(int targ) // constructor
             {
                 this.targ = targ;
-                this.pflist = primefactors();
-                
+                this.pflist = primefactors();       
             }
 
             public List<long> primefactors()
             {
-                Console.WriteLine();
-                Console.Write("\nPrime factors of {0} = ", this.targ);
+                //Console.WriteLine();
+                //Console.Write("\nPrime factors of {0} = ", this.targ);
                 for (int i = 1; i < targ; i++)
                 {
                     if (targ % i == 0 & primefunc(i) == true) // check if prime factor
@@ -52,45 +51,68 @@ namespace Scratch2name
                     }
                 }
                 //Console.Write("{0}:", target);
-                foreach (var pf in pflist) { Console.Write("{0}, ", pf); }; // prime factors
+                //foreach (var pf in pflist) { Console.Write("{0}, ", pf); }; // prime factors
                 // make set
-                Console.Write("| Set: ");
+                //Console.Write("| Set: ");
                 foreach (var f in pflist) { this.pset.Add(f); }; // have to wait till list is full before making a set
-                foreach (var v in pset) { Console.Write("{0}, ", v); }; // print set
+                //foreach (var v in pset) { Console.Write("{0}, ", v); }; // print set
                 return pflist; // return list
             }
         }
-
-        //TODO
-            // iterate lists and make a set
-            // count occurences in set
-            // multiply max occurences
-            // fin
 
 
         static void Main()
         {
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
-            SortedSet<long> mainpset = new SortedSet<long>();
-            List<Coords> coordlist = new List<Coords>();
+            SortedSet<long> mainpset = new SortedSet<long>(); // set of primes
+            List<int> occlist = new List<int>();  // list of occurences - cross references mainpset
+            List<Coords> coordlist = new List<Coords>();  // list of class instances
             for (int i = 1; i < 21; i++)
             {
                 coordlist.Add(new Coords(i));
             }
             foreach (var c in coordlist) { foreach (var d in c.pset) { mainpset.Add(d); } };
-            //coordlist.Add(new Coords(456));
-            //foreach (var p in coordlist) { Console.Write("\n{0} , {1}", p.x, p.y); }
-            //foreach (var p in coordlist) { foreach (var x in p.pflist) { Console.Write("{0}, ", x); }; }
+            //Console.Write("\nPrime Set: ");
+            //foreach (var e in mainpset) { Console.Write("{0}, ", e); };
+            //Console.Write("\n");
+            int counter = 0;
+            int highcount = 0;
+            foreach (var p in mainpset) // set of primes
+            {
+                counter = 0;
+                highcount = 0;
+                foreach (var c in coordlist) // list of instances
+                {
+                    counter = 0;  // reset the counter at start of each instance
+                    foreach (var n in c.pflist) // each instances list of prime factors
+                    {
+                        if (n == p) // is this the prime we are looking for
+                        {
+                            counter += 1;
+                            if (counter > highcount)
+                            {
+                                highcount = counter;
+                            }
+                        }
+                    };
+                };
+                occlist.Add(highcount);  // track highest number of occurences
+            };
+            //Console.Write("Occurences: ");
+            //foreach (var h in occlist) { Console.Write(h); };
+            int newcount = 0;
+            double total = 1;
+            long z = 0;
+            foreach (var x in mainpset)
+            {
+                z = occlist[newcount];
+                total *= Math.Pow(x, z); // multiply by our number raised to the power of occurences (pow returns a double)
+                newcount += 1;
+            };
+            Console.WriteLine("\nAnswer: {0}", total);
             watch.Stop();
             Console.WriteLine("\nTook: {0} ticks", watch.ElapsedTicks);
-            Console.Write("\nPrime Set: ");
-            foreach (var e in mainpset) { Console.Write("{0}, ", e); };
-            Console.Write("\n");
-
-            // make a list, 0 is first in set, 1 is second in set, 2 so on...
-            // in this case, index 0 is 2, index 1 is 3, index 2 is 5
-
         }
     }
 }
