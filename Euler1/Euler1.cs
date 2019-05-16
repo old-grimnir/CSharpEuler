b64064baa92204b117401e6616f8c4e1ae111208
ccnelson <ccnelson@mail.com>
2019-05-16 16:13:19 +0100

    better prob 1 and 5

--- Euler1/Euler1.cs
+++ Euler1/Euler1.cs
@@ -2,6 +2,7 @@
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. 
 (Answer = 233168) */
+// this version uses inclusion exclusion principle
 
 using System;
 using System.Diagnostics;
@@ -13,17 +14,26 @@ namespace Euler1name
         static void Main(string[] args)
         {
             var watch = new System.Diagnostics.Stopwatch();
-            int total = 0;
+            double sumofthrees = 0;
+            double sumoffives = 0;
+            double sumoffifteens = 0;
+            double finalanswer = 0;
             watch.Start();
-            for (int i = 1; i < 1000; i++)
+            for (int i = 3; i < 1000; i += 3)
             {
-                if ((i % 3 == 0) || (i % 5 == 0))  // short circuit 'OR'
-                {
-                    total += i;
-                }
+                sumofthrees += i;
             }
-            watch.Stop(); 
-            Console.WriteLine("Answer: {0}", total);
+            for (int j = 5; j < 1000; j += 5)
+            {
+                sumoffives += j;
+            }
+            for (int k = 15; k < 1000; k += 15)
+            {
+                sumoffifteens += k;
+            }
+            finalanswer = (sumofthrees + sumoffives) - sumoffifteens;
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", finalanswer);
             Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
         }
     }
--- Euler5/Euler5.cs
+++ Euler5/Euler5.cs
@@ -3,30 +3,108 @@ each of the numbers from 1 to 10 without any remainder.
 what is the smallest positive number that is evenly 
 divisible by all of the numbers from 1 to 20?
 (Answer: 232792560) */
-// this is not ideal ( 210107 ticks) 
-// option A: prime factors to find LCM
-// option B: find GCD to find LCM
+// this version uses multiplication of prime factors to find LCM
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Euler5name
 {
     class Euler5
     {
-        static void Main(string[] args)
+        static Boolean primefunc(int inputNo)
+        {
+            if (inputNo <= 1) return false; // 1 isnt prime
+            if (inputNo == 2) return true; // 2 is prime
+            if (inputNo % 2 == 0) return false; // evens arent prime
+            var boundary = (int)Math.Floor(Math.Sqrt(inputNo)); // set boundary
+            for (int i = 3; i <= boundary; i += 2) // increment thru odds
+            {
+                if (inputNo % i == 0) return false;
+            }
+            return true;
+        }
+
+
+        class Coords
+        {
+            public int targ; // number in question
+            public List<long> pflist = new List<long>(); // list for prime factors
+            public SortedSet<long> pset = new SortedSet<long>(); // set for unique prime factors
+
+            public Coords(int targ) // constructor
+            {
+                this.targ = targ; // supplied target
+                this.pflist = primefactors(); // populate list via method
+            }
+
+            public List<long> primefactors()
+            {
+                for (int i = 1; i < targ; i++)
+                {
+                    if (targ % i == 0 & primefunc(i) == true) // check if prime factor
+                    {
+                        pflist.Add(i); // add to list
+                        targ = targ / i;
+                        i = 1; // reset as we just divided
+                    }
+                    if (primefunc(targ) == true) // check if target yet prime
+                    {
+                        pflist.Add(targ); // add to list
+                        break; // reached end
+                    }
+                }
+                foreach (var f in pflist) { this.pset.Add(f); }; // have to wait till list is full before making a set
+                return pflist; // return list
+            }
+        }
+
+        static void Main()
         {
             var watch = new System.Diagnostics.Stopwatch();
-            int i = 20;
             watch.Start();
-            while (i % 3 != 0 || i % 4 != 0 || i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 || i % 11 != 0 ||
-            i % 12 != 0 || i % 13 != 0 || i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 || i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
+            SortedSet<long> mainpset = new SortedSet<long>(); // main set of primes
+            List<int> occlist = new List<int>();  // list of occurences - cross references mainpset
+            List<Coords> coordlist = new List<Coords>();  // list for class instances
+            for (int i = 1; i < 21; i++)
             {
-                i += 20; // increments of 20, highest number we need divisable. removed 2, 5, and 10. 
+                coordlist.Add(new Coords(i)); // add instances with target from 1 to 20
             }
+            foreach (var c in coordlist) { foreach (var d in c.pset) { mainpset.Add(d); } }; // populate main set
+            int counter = 0; // counter for occurences
+            int highcount = 0; // track highest occurences
+            foreach (var p in mainpset) // set of primes
+            {
+                highcount = 0; // reset counter
+                foreach (var c in coordlist) // list of instances
+                {
+                    counter = 0;  // reset the counter at start of each instance
+                    foreach (var n in c.pflist) // each instances list of prime factors
+                    {
+                        if (n == p) // is this the prime we are looking for
+                        {
+                            counter += 1;
+                            if (counter > highcount)
+                            {
+                                highcount = counter;
+                            }
+                        }
+                    };
+                };
+                occlist.Add(highcount);  // track highest number of occurences
+            };
+            int newcount = 0; // track position in occurence list
+            double total = 1; // total so far (product needs to start as 1)
+            long z = 0;
+            foreach (var x in mainpset)
+            {
+                total *= Math.Pow(x, occlist[newcount]); // multiply by our number raised to the power of occurences (pow returns a double)
+                newcount += 1; // increment occuence count
+            };
+            Console.WriteLine("\nAnswer: {0}", total);
             watch.Stop();
-            Console.WriteLine("Answer: {0}", i);
-            Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
+            Console.WriteLine("\nTook: {0} ticks", watch.ElapsedTicks);
         }
     }
 }
