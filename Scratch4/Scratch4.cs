e4f133f379c0b4752270b155d9e55070d5befb1d
ccnelson <ccnelson@mail.com>
2019-05-23 17:32:42 +0100

    cleared scratch

--- Scratch4/Scratch4.cs
+++ Scratch4/Scratch4.cs
@@ -7,31 +7,8 @@ namespace Scratch4name
     {
         static void Main(string[] args)
         {
-            var watch = new System.Diagnostics.Stopwatch();
-            watch.Start();
-            int num = 15485863;
-            int range = num + 1;
-            Dictionary<int, bool> newdic = new Dictionary<int, bool>();
-            for (int i = 0; i < range; i++)
-            {
-                newdic[i] = true;
-            }
-            newdic[0] = false;
-            newdic[1] = false;
-            for (int j = 2; j < range; j++)
-            {
-                if (newdic[j] == true)
-                {
-                    for (int k = j + j; k < range; k += j)
-                    {
-                        newdic[k] = false;
-                    }
-                }
-            }
-            Console.WriteLine("Is prime: {0}", newdic[num]);
-            watch.Stop();
-            Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
-            Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
+
+            Console.WriteLine("dlrow olleh");
         }
     }
 }
