9122e96703bdc171c86c1a71f18eb052dc121d8d
ccnelson <ccnelson@mail.com>
2019-05-28 20:38:43 +0100

    new linq versions

---
+++ Euler01_linq/Euler01_linq.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler01_linqname
+{
+    class Euler01_linq
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int[] vals = Enumerable.Range(1, 999).ToArray(); // array comprehension
+            int total = 0;
+
+            var lq = from v in vals  // actual linq query
+                     where (v % 3 == 0) || (v % 5 == 0)
+                     select v;
+
+            foreach (var sv in lq) // use the linq object
+            {
+                total += sv;
+            };
+
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", total);
+            Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
+            Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Euler02_linq/Euler02_linq.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler02_linqname
+{
+    class Euler02_linq
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            // Using long just to avoid having to change if we want a higher limit :)
+            IEnumerable<long> Fibonacci()
+            {
+                long current = 0;
+                long next = 1;
+                long temp;
+                while (true)
+                {
+                    yield return current; // returns current no.
+                    temp = next; // temporaily store next no.
+                    next = current + next; // prepare next no.
+                    current = temp; // assign next no.
+                }
+            }
+
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            long evenSum = Fibonacci().TakeWhile(x => x < 4000000L)  // 'x =>' seems to be iterator
+                          .Where(y => y % 2 == 0)  // second iterator declared here
+                          .Sum();
+
+            //Console.WriteLine(evenSum);
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", evenSum);
+            Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
+            Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
