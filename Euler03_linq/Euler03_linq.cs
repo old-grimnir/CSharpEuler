666aa03ec4bb6f1717f00e2007f526575c97a985
ccnelson <ccnelson@mail.com>
2019-05-29 21:01:52 +0100

    new linq sols

---
+++ Euler03_linq/Euler03_linq.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Euler03_linqname
+{
+    class Euler03_linq
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Euler04_linq/Euler04_linq.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler04_linqname
+{
+    class Euler04_linq
+    {
+        static bool IsReversable(int target)
+        {
+            var digits = target.ToString().ToCharArray();
+            return digits.SequenceEqual(digits.Reverse());
+        }
+
+
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int temp = 0;
+            int sol = 0;
+
+            for (int i = 1000; i > 900; i--)
+            {
+                for (int j = 1000; j > 900; j--)
+                {
+                    temp = i * j;
+                    if (IsReversable(temp) == true && temp > sol)
+                    {
+                        sol = temp;
+                    }
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Answer: {0}", sol);
+            Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
+            Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
